304648b3930b77445690fad72e5a09cfda705d33
HJ0216 <6120hj@gmail.com>
2024-09-18 07:40:59 +0900

    #-- Controller 테스트 코드 추가

--- MSGS/src/main/java/com/msgs/global/common/error/ErrorCode.java
+++ MSGS/src/main/java/com/msgs/global/common/error/ErrorCode.java
@@ -10,25 +10,18 @@ import java.util.stream.Stream;
 
 @Getter
 public enum ErrorCode {
-
-    // 회원
-    DUPLICATED_EMAIL(HttpStatus.BAD_REQUEST, "이미 존재하는 이메일 입니다."),
-    NOT_EQUAL_PASSWORD(HttpStatus.BAD_REQUEST,"입력한 비밀번호가 상이합니다."),
-    CHECK_LOGIN_ID_OR_PASSWORD(HttpStatus.NOT_FOUND, "아이디 또는 비밀번호를 확인해주세요."),
-    NOT_FOUND_MEMBER(HttpStatus.NOT_FOUND, "존재하지 않는 회원입니다."),
-    LOGOUT_MEMBER(HttpStatus.NOT_FOUND, "로그아웃한 회원입니다."),
-    INVALID_ACCESS_TOKEN(HttpStatus.NOT_FOUND, "유효하지 않은 Access Token입니다."),
-    INVALID_REFRESH_TOKEN(HttpStatus.NOT_FOUND, "유효하지 않은 Refresh Token입니다."),
-
+    // 회원 가입
     EMAIL_VALIDATION(HttpStatus.BAD_REQUEST, "이메일 형식이 맞지 않습니다."),
+    DUPLICATED_EMAIL(HttpStatus.BAD_REQUEST, "이미 존재하는 이메일 입니다."),
     PASSWORD_VALIDATION(HttpStatus.BAD_REQUEST, "비밀번호 형식이 맞지 않습니다."),
     NICKNAME_VALIDATION(HttpStatus.BAD_REQUEST, "닉네임 형식이 맞지 않습니다."),
     PHONE_NUMBER_VALIDATION(HttpStatus.BAD_REQUEST, "휴대폰 형식이 맞지 않습니다."),
     PASSWORD_CONFIRM_VALIDATION(HttpStatus.BAD_REQUEST, "비밀번호와 비밀번호 확인이 맞지 않습니다."),
 
 
-    // 권한
-    NOT_FOUND_AUTHORITY(HttpStatus.NOT_FOUND, "존재하지 않는 권한입니다."),
+    // 로그인
+    CHECK_LOGIN_ID_OR_PASSWORD(HttpStatus.UNAUTHORIZED, "아이디 또는 비밀번호를 확인해주세요."),
+    NOT_FOUND_MEMBER(HttpStatus.NOT_FOUND, "존재하지 않는 회원입니다."),
 
 
     // JWT
@@ -37,7 +30,12 @@ public enum ErrorCode {
     UNSUPPORTED_JWT(HttpStatus.BAD_REQUEST, "지원하지 않은 JWT 서명입니다."),
     ILLEGAL_STATE_JWT(HttpStatus.BAD_REQUEST, "JWT 처리 중 오류가 발생하였습니다."),
     VALID_ACCESS_TOKEN(HttpStatus.UNAUTHORIZED, "Access Token의 유효기간이 남아있습니다."),
+    LOGOUT_MEMBER(HttpStatus.NOT_FOUND, "로그아웃한 회원입니다."),
+    INVALID_ACCESS_TOKEN(HttpStatus.NOT_FOUND, "유효하지 않은 Access Token입니다."),
+    INVALID_REFRESH_TOKEN(HttpStatus.NOT_FOUND, "유효하지 않은 Refresh Token입니다."),
+    NOT_FOUND_AUTHORITY(HttpStatus.NOT_FOUND, "존재하지 않는 권한입니다."),
     ;
+    
 
     private final HttpStatus httpStatus;
     private final String message;
--- MSGS/src/test/java/com/msgs/domain/user/controller/UserControllerTest.java
+++ MSGS/src/test/java/com/msgs/domain/user/controller/UserControllerTest.java
@@ -1,8 +1,12 @@
 package com.msgs.domain.user.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.msgs.domain.user.dto.LoginRequestDTO;
 import com.msgs.domain.user.dto.SignUpRequestDTO;
 import com.msgs.domain.user.service.UserService;
+import com.msgs.global.common.error.BusinessException;
+import com.msgs.global.common.error.ErrorCode;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,29 +16,46 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.refEq;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
-@AutoConfigureMockMvc
+@AutoConfigureMockMvc(addFilters = false)
+//@AutoConfigureMockMvc
 class UserControllerTest {
     @Autowired MockMvc mockMvc;
     @Autowired ObjectMapper objectMapper;
     @MockBean UserService userService;
 
+//    @BeforeEach
+//    void setup() {
+//        SignUpRequestDTO signUpDto = SignUpRequestDTO.builder()
+//                .status("M")
+//                .email("temp@email.com")
+//                .phone("01023698745")
+//                .nickname("name")
+//                .password("temp123!")
+//                .confirmPassword("temp123!")
+//                .build();
+//
+//        userService.create(signUpDto);
+//    }
+
     @Test
-    @DisplayName("Controller: 회원 가입")
-    void create() throws Exception {
+    @DisplayName("Controller: 회원 가입 성공")
+    void createSuccess() throws Exception {
         // given
         SignUpRequestDTO signUpDto = SignUpRequestDTO.builder()
                 .status("M")
-                .email("Tcontroller@email.com")
+                .email("new@email.com")
                 .phone("01023698741")
-                .nickname("Tname")
-                .password("test123!")
-                .confirmPassword("test123!")
+                .nickname("name")
+                .password("newnew123!")
+                .confirmPassword("newnew123!")
                 .build();
 
         // when // then
@@ -48,7 +69,94 @@ class UserControllerTest {
     }
 
     @Test
-    void login() {
+    @DisplayName("Controller: 회원 가입 실패, 중복된 이메일")
+    void createFailDuplicateEmail() throws Exception {
+        // given
+        SignUpRequestDTO signUpDto = SignUpRequestDTO.builder()
+                .status("M")
+                .email("temp@email.com")
+                .phone("01023698745")
+                .nickname("name")
+                .password("temp123!")
+                .confirmPassword("temp123!")
+                .build();
+
+        // when // then
+        doThrow(new BusinessException(ErrorCode.DUPLICATED_EMAIL))
+                .when(userService).create(any(SignUpRequestDTO.class));
+
+        mockMvc.perform(post("/api/v2/users/new")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(signUpDto)))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.errorMessage").value("이미 존재하는 이메일 입니다."));
+
+        // 회원 생성 메소드가 호출되었는지 확인
+        verify(userService).create(refEq(signUpDto));
+    }
+
+    @Test
+    @DisplayName("Controller: 로그인 성공")
+    void loginSuccess() throws Exception{
+        // given
+        LoginRequestDTO loginDto = LoginRequestDTO.builder()
+                .email("temp@email.com")
+                .password("temp123!")
+                .build();
+
+        // when // then
+        mockMvc.perform(post("/api/v2/users/login")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(loginDto)))
+                .andExpect(status().isOk());
+
+        verify(userService).login(refEq(loginDto));
+    }
+
+    @Test
+    @DisplayName("Controller: 로그인 실패 - 가입하지 않은 회원")
+    void loginFailUnjoinUser() throws Exception {
+        // given
+        LoginRequestDTO loginDto = LoginRequestDTO.builder()
+                .email("tem_p@email.com")
+                .password("temp123!")
+                .build();
+
+        when(userService.login(any(LoginRequestDTO.class)))
+                .thenThrow(new BusinessException(ErrorCode.NOT_FOUND_MEMBER));
+
+
+        // when // then
+        mockMvc.perform(post("/api/v2/users/login")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(loginDto)))
+                .andExpect(status().isNotFound())
+                .andExpect(jsonPath("$.errorMessage").value("존재하지 않는 회원입니다."));
+
+        verify(userService).login(refEq(loginDto));
+    }
+
+    @Test
+    @DisplayName("Controller: 로그인 실패 - 잘못된 비밀번호")
+    void loginFailWrongPassword() throws Exception {
+        // given
+        LoginRequestDTO loginDto = LoginRequestDTO.builder()
+                .email("temp@email.com")
+                .password("temp123_!")
+                .build();
+
+        when(userService.login(any(LoginRequestDTO.class)))
+                .thenThrow(new BusinessException(ErrorCode.CHECK_LOGIN_ID_OR_PASSWORD));
+
+
+        // when // then
+        mockMvc.perform(post("/api/v2/users/login")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(loginDto)))
+                .andExpect(status().isUnauthorized())
+                .andExpect(jsonPath("$.errorMessage").value("아이디 또는 비밀번호를 확인해주세요."));
+
+        verify(userService).login(refEq(loginDto));
     }
 
     @Test
