03c8190b7747911fe2210ba2d1bd637e8f358da1
HJ0216 <6120hj@gmail.com>
2024-09-28 23:34:18 +0900

    ✨Feat: #10 회원가입 관련 DTO Validation 처리 방식 변경에 따른 Error Reponse 방식 변경 - 회원가입 정보 검증 방식: ValidationUtils -> DTO 내 @Valdation

--- MSGS/src/main/java/com/msgs/global/common/error/GlobalExceptionHandler.java
+++ MSGS/src/main/java/com/msgs/global/common/error/GlobalExceptionHandler.java
@@ -1,14 +1,83 @@
 package com.msgs.global.common.error;
 
+import java.util.List;
+import java.util.stream.Collectors;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.reactive.result.method.annotation.ResponseEntityExceptionHandler;
 
 @RestControllerAdvice
-public class GlobalExceptionHandler {
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
   @ExceptionHandler(BusinessException.class)
-  public ResponseEntity<ErrorResponse> businessExceptionHandler(BusinessException e) {
-    return ErrorResponse.toResponseEntity(e.getErrorCode());
+  public ResponseEntity<Object> handleBusiness(BusinessException e) {
+    ErrorCode errorCode = e.getErrorCode();
+    return handleExceptionInternal(errorCode);
+  }
+
+  @ExceptionHandler(IllegalArgumentException.class)
+  public ResponseEntity<Object> handleIllegalArgument(IllegalArgumentException e) {
+    ErrorCode errorCode = CommonErrorCode.BAD_REQUEST;
+    return handleExceptionInternal(errorCode, e.getMessage());
+  }
+
+  @ExceptionHandler(MethodArgumentNotValidException.class)
+  public ResponseEntity<Object> handleMethodArgumentNotValid(
+      MethodArgumentNotValidException e) {
+    ErrorCode errorCode = CommonErrorCode.BAD_REQUEST;
+    return handleExceptionInternal(e, errorCode);
+  }
+
+  @ExceptionHandler({Exception.class})
+  public ResponseEntity<Object> handleAllException(Exception ex) {
+    ErrorCode errorCode = CommonErrorCode.INTERNAL_SERVER_ERROR;
+    return handleExceptionInternal(errorCode);
+  }
+
+  private ResponseEntity<Object> handleExceptionInternal(ErrorCode errorCode) {
+    return ResponseEntity.status(errorCode.getHttpStatus())
+                         .body(makeErrorResponse(errorCode));
+  }
+
+  private ErrorResponse makeErrorResponse(ErrorCode errorCode) {
+    return ErrorResponse.builder()
+                        .code(errorCode.name())
+                        .message(errorCode.getMessage())
+                        .build();
+  }
+
+  private ResponseEntity<Object> handleExceptionInternal(ErrorCode errorCode, String message) {
+    return ResponseEntity.status(errorCode.getHttpStatus())
+                         .body(makeErrorResponse(errorCode, message));
+  }
+
+  private ErrorResponse makeErrorResponse(ErrorCode errorCode, String message) {
+    return ErrorResponse.builder()
+                        .code(errorCode.name())
+                        .message(message)
+                        .build();
+  }
+
+  private ResponseEntity<Object> handleExceptionInternal(BindException e, ErrorCode errorCode) {
+    return ResponseEntity.status(errorCode.getHttpStatus())
+                         .body(makeErrorResponse(e, errorCode));
+  }
+
+  private ErrorResponse makeErrorResponse(BindException e, ErrorCode errorCode) {
+    List<ErrorResponse.ValidationError> validationErrorList = e.getBindingResult()
+                                                               .getFieldErrors()
+                                                               .stream()
+                                                               .map(
+                                                                   ErrorResponse.ValidationError::of)
+                                                               .collect(Collectors.toList());
+
+    return ErrorResponse.builder()
+                        .code(errorCode.name())
+                        .message(errorCode.getMessage())
+                        .errors(validationErrorList)
+                        .build();
   }
 }
