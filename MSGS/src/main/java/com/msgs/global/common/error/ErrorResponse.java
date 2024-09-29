304bec8e204a67718e7dd3de95fbd607450ea8eb
HJ0216 <6120hj@gmail.com>
2024-09-29 14:54:50 +0900

    💡Comment: #-- ErrorResponse 관련 주석 추가

--- MSGS/src/main/java/com/msgs/global/common/error/ErrorResponse.java
+++ MSGS/src/main/java/com/msgs/global/common/error/ErrorResponse.java
@@ -16,17 +16,21 @@ public class ErrorResponse {
   private final String message;
 
   @JsonInclude(JsonInclude.Include.NON_EMPTY)
+  // errors 필드가 비어 있지 않을 때만 JSON 직렬화에 포함
   private final List<ValidationError> errors;
+  // 클라이언트 요청에서 특정 필드에 잘못된 데이터가 들어온 경우 유효성 검사 오류 저장
 
   @Getter
   @Builder
   @RequiredArgsConstructor
   public static class ValidationError {
+    // @Valid를 사용했을 때 에러가 발생한 경우 어느 필드에서 에러가 발생했는지 응답을 위한 내부 정적 클래스
 
     private final String field;
     private final String message;
 
     public static ValidationError of(final FieldError fieldError) {
+      // FieldError: Spring에서 유효성 검사가 실패한 필드에 대한 정보를 제공하는 클래스
       return ValidationError.builder()
                             .field(fieldError.getField())
                             .message(fieldError.getDefaultMessage())
