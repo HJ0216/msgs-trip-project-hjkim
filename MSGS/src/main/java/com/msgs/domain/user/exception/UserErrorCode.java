6bc4261edb6544f1916674f2ae447f265ba8371b
HJ0216 <6120hj@gmail.com>
2024-10-18 18:33:21 +0900

    🧹Chore: #49 Http Status 에러 상황에 맞춰 수정

--- MSGS/src/main/java/com/msgs/domain/user/exception/UserErrorCode.java
+++ MSGS/src/main/java/com/msgs/domain/user/exception/UserErrorCode.java
@@ -10,7 +10,7 @@ import org.springframework.http.HttpStatus;
 public enum UserErrorCode implements ErrorCode {
   // 회원 가입
   EMAIL_VALIDATION(HttpStatus.BAD_REQUEST, "이메일 형식이 맞지 않습니다."),
-  DUPLICATED_EMAIL(HttpStatus.BAD_REQUEST, "이미 존재하는 이메일 입니다."),
+  DUPLICATED_EMAIL(HttpStatus.CONFLICT, "이미 존재하는 이메일 입니다."),
   PASSWORD_VALIDATION(HttpStatus.BAD_REQUEST, "비밀번호 형식이 맞지 않습니다."),
   PASSWORD_CONFIRM_VALIDATION(HttpStatus.BAD_REQUEST, "비밀번호와 비밀번호 확인이 맞지 않습니다."),
   PHONE_NUMBER_VALIDATION(HttpStatus.BAD_REQUEST, "휴대폰 형식이 맞지 않습니다."),
@@ -23,14 +23,14 @@ public enum UserErrorCode implements ErrorCode {
 
   // JWT
   MALFORMED_JWT(HttpStatus.BAD_REQUEST, "잘못된 JWT 형식입니다."),
-  EXPIRED_JWT(HttpStatus.BAD_REQUEST, "만료된 JWT 토큰입니다."),
+  EXPIRED_JWT(HttpStatus.UNAUTHORIZED, "만료된 JWT 토큰입니다."),
   UNSUPPORTED_JWT(HttpStatus.BAD_REQUEST, "지원하지 않은 JWT 서명입니다."),
-  ILLEGAL_STATE_JWT(HttpStatus.BAD_REQUEST, "JWT 처리 중 오류가 발생하였습니다."),
-  VALID_ACCESS_TOKEN(HttpStatus.UNAUTHORIZED, "Access Token의 유효기간이 남아있습니다."),
-  LOGOUT_MEMBER(HttpStatus.NOT_FOUND, "로그아웃한 회원입니다."),
-  INVALID_ACCESS_TOKEN(HttpStatus.NOT_FOUND, "유효하지 않은 Access Token입니다."),
-  INVALID_REFRESH_TOKEN(HttpStatus.NOT_FOUND, "유효하지 않은 Refresh Token입니다."),
-  NOT_FOUND_AUTHORITY(HttpStatus.NOT_FOUND, "존재하지 않는 권한입니다."),
+  ILLEGAL_STATE_JWT(HttpStatus.INTERNAL_SERVER_ERROR, "JWT 처리 중 오류가 발생하였습니다."),
+  VALID_ACCESS_TOKEN(HttpStatus.OK, "Access Token의 유효기간이 남아있습니다."),
+  LOGOUT_MEMBER(HttpStatus.UNAUTHORIZED, "로그아웃한 회원입니다."),
+  INVALID_ACCESS_TOKEN(HttpStatus.UNAUTHORIZED, "유효하지 않은 Access Token입니다."),
+  INVALID_REFRESH_TOKEN(HttpStatus.UNAUTHORIZED, "유효하지 않은 Refresh Token입니다."),
+  NOT_FOUND_AUTHORITY(HttpStatus.FORBIDDEN, "존재하지 않는 권한입니다."),
   ;
 
   private final HttpStatus httpStatus;
