25cd14e755d51c12cb552ce7184de6c54f6259b3
HJ0216 <6120hj@gmail.com>
2024-11-22 11:41:07 +0900

    🐛Fix: #51 요청 값이 JSON이 아닐 경우, 오류 처리 추가

--- MSGS/src/main/java/com/msgs/global/common/error/GlobalExceptionHandler.java
+++ MSGS/src/main/java/com/msgs/global/common/error/GlobalExceptionHandler.java
@@ -1,25 +1,33 @@
 package com.msgs.global.common.error;
 
+import com.msgs.domain.user.exception.UserErrorCode;
 import java.util.List;
-import java.util.stream.Collectors;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.validation.BindException;
+import org.springframework.web.HttpMediaTypeNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.reactive.result.method.annotation.ResponseEntityExceptionHandler;
 
+@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
   @ExceptionHandler(BusinessException.class)
-  public ResponseEntity<Object> handleBusiness(BusinessException e) {
+  public static ResponseEntity<Object> handleBusiness(BusinessException e) {
+    log.error("Business Exception: {}", e.getMessage());
+
     ErrorCode errorCode = e.getErrorCode();
     return handleExceptionInternal(errorCode);
   }
 
   @ExceptionHandler(IllegalArgumentException.class)
   public ResponseEntity<Object> handleIllegalArgument(IllegalArgumentException e) {
+    log.error("IllegalArgument Exception: {}", e.getMessage());
+
     ErrorCode errorCode = CommonErrorCode.INVALID_PARAMETER;
     return handleExceptionInternal(errorCode, e.getMessage());
   }
@@ -27,22 +35,43 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
   @ExceptionHandler(MethodArgumentNotValidException.class)
   public ResponseEntity<Object> handleMethodArgumentNotValid(
       MethodArgumentNotValidException e) {
+    log.error("Validation Exception: {}", e.getMessage());
+
     ErrorCode errorCode = CommonErrorCode.INVALID_PARAMETER;
     return handleExceptionInternal(e, errorCode);
   }
 
+  @ExceptionHandler(AccessDeniedException.class)
+  public ResponseEntity<Object> handleAccessDenied(AccessDeniedException e) {
+    log.error("Access Denied Exception: {}", e.getMessage());
+
+    ErrorCode errorCode = UserErrorCode.INVALID_ACCESS_TOKEN;
+    return handleExceptionInternal(errorCode);
+  }
+
+  @ExceptionHandler(HttpMediaTypeNotSupportedException.class)
+  public ResponseEntity<Object> handleHttpMediaTypeNotSupported(
+      HttpMediaTypeNotSupportedException e) {
+    log.error("Unsupported Media Type Exception: {}", e.getMessage());
+
+    ErrorCode errorCode = UserErrorCode.UNSUPPORTED_MEDIA_TYPE;
+    return handleExceptionInternal(errorCode);
+  }
+
   @ExceptionHandler({Exception.class})
-  public ResponseEntity<Object> handleAllException(Exception ex) {
+  public static ResponseEntity<Object> handleAllException(Exception e) {
+    log.error("Internal Server Error", e);
+
     ErrorCode errorCode = CommonErrorCode.INTERNAL_SERVER_ERROR;
     return handleExceptionInternal(errorCode);
   }
 
-  private ResponseEntity<Object> handleExceptionInternal(ErrorCode errorCode) {
+  private static ResponseEntity<Object> handleExceptionInternal(ErrorCode errorCode) {
     return ResponseEntity.status(errorCode.getHttpStatus())
                          .body(makeErrorResponse(errorCode));
   }
 
-  private ErrorResponse makeErrorResponse(ErrorCode errorCode) {
+  private static ErrorResponse makeErrorResponse(ErrorCode errorCode) {
     return ErrorResponse.builder()
                         .code(errorCode.name())
                         .message(errorCode.getMessage())
@@ -72,8 +101,7 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
                                                                .stream()
                                                                .map(
                                                                    ErrorResponse.ValidationError::of)
-                                                               .collect(Collectors.toList());
-
+                                                               .toList();
     return ErrorResponse.builder()
                         .code(errorCode.name())
                         .message(errorCode.getMessage())
